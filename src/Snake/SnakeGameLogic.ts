b27a6e6a30585188179eef27c2e2a90e31a3e23e
Paige Sun <paige.sun.dev@gmail.com>
2022-12-01 13:58:45 -0800

    minor: Make playStatus in SnakeGameLogic have only public getter.

--- src/Snake/SnakeGameLogic.ts
+++ src/Snake/SnakeGameLogic.ts
@@ -6,25 +6,29 @@ import { SnakePlayStatus } from './SnakeGame';
 
 export default class SnakeGameLogic {
   #gridSize: GridSize;
-  playStatus: SnakePlayStatus = 'waiting';
+  #playStatus: SnakePlayStatus = 'waiting';
   snake: Snake;
   fruit: Fruit;
+
+  get playStatus() {
+    return this.#playStatus;
+  }
 
   constructor(gridSize: GridSize) {
     this.#gridSize = gridSize;
     this.snake = Snake.createRandom(gridSize);
     this.fruit = new Fruit();
-    this.restartGame();
+    this.#restartGame();
   }
 
-  restartGame() {
+  #restartGame() {
     this.snake = Snake.createRandom(this.#gridSize);
     this.fruit = new Fruit();
-    this.#generateNewFruit();
+    this.fruit.generateNewPosition(this.#gridSize, this.snake);
   }
 
   tick() {
-    if (this.playStatus === 'playing') {
+    if (this.#playStatus === 'playing') {
       const newSnake = this.snake.tick();
 
       if (GridPositionEqual(this.snake.headPosition, this.fruit.position)) {
@@ -34,7 +38,7 @@ export default class SnakeGameLogic {
 
       const hasCollision = newSnake.hasCollision(this.#gridSize);
       if (hasCollision) {
-        this.playStatus = 'lost';
+        this.#playStatus = 'lost';
       } else {
         this.snake = newSnake;
       }
@@ -46,14 +50,11 @@ export default class SnakeGameLogic {
   }
 
   onSpaceKeyPressed() {
-    if (this.playStatus === 'lost') {
-      this.restartGame();
+    if (this.#playStatus === 'lost') {
+      this.#restartGame();
+      this.#playStatus = 'playing';
+    } else if (this.playStatus === 'waiting') {
+      this.#playStatus = 'playing';
     }
-
-    this.playStatus = 'playing';
-  }
-
-  #generateNewFruit() {
-    this.fruit.generateNewPosition(this.#gridSize, this.snake);
   }
 }
