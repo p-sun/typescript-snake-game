5113fe480866bdeee09f554a06f30e07c98011a5
Paige Sun <paige.sun.dev@gmail.com>
2022-12-04 13:07:18 -0800

    change: Reduce cell size so it's easier to share on stackblitz.

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ import SnakeGame from './src/SnakeGame/SnakeGame';
 const appRoot = document.getElementById('root');
 if (appRoot) {
   const canvas = HTMLCanvas.createInRootElement(appRoot);
-  const gridSize = { rowCount: 24, columnCount: 24 };
-  const cellSize = new Vec2(30, 30);
+  const gridSize = { rowCount: 18, columnCount: 18 };
+  const cellSize = new Vec2(22, 22);
   const snakeGame = new SnakeGame(canvas, gridSize, cellSize);
   snakeGame.run(12);
 
