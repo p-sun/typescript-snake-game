025e1209341745bbd6a980b066820a485ad1c260
Paige Sun <paige.sun.dev@gmail.com>
2023-08-10 20:04:31 -0400

    minor: Move TriangleRenderHelpers utility methods to its own file.

--- src/FoldTriangles/renderers/TriangleRenderHelpers.ts
+++ src/FoldTriangles/renderers/TriangleRenderHelpers.ts
@@ -1,5 +1,6 @@
 import Rect from '../../GenericModels/Rect';
 import Vec2 from '../../GenericModels/Vec2';
+import { TriangleRotation } from '../models/TrianglesGameLogic';
 
 // Corner in Rectangle in Canvas
 export type Corner = 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight';
@@ -16,21 +17,24 @@ function vertForCorner(c: Corner, rect: Rect) {
   throw new Error(`Invalid corner: ${c}`);
 }
 
-// Triangle Position
-export type TrianglePosition = Corner;
-function cornersInTriangle(c: TrianglePosition): Corner[] {
-  if (c === 'topLeft') {
-    return ['topRight', 'topLeft', 'bottomLeft'];
-  } else if (c === 'topRight') {
+// Triangle Rotation
+function cornersInTriangle(rot: TriangleRotation): Corner[] {
+  if (rot === 1) {
+    // topRight
     return ['topRight', 'topLeft', 'bottomRight'];
-  } else if (c === 'bottomLeft') {
-    return ['topLeft', 'bottomLeft', 'bottomRight'];
-  } else if (c === 'bottomRight') {
+  } else if (rot === 2) {
+    // bottomRight
     return ['topRight', 'bottomLeft', 'bottomRight'];
+  } else if (rot === 3) {
+    // bottomLeft
+    return ['topLeft', 'bottomLeft', 'bottomRight'];
+  } else if (rot === 4) {
+    // topLeft
+    return ['topRight', 'topLeft', 'bottomLeft'];
   }
-  throw new Error(`Invalid corner: ${c}`);
+  throw new Error(`Invalid corner: ${rot}`);
 }
 
-export function getTriangleVerts(trianglePos: TrianglePosition, rect: Rect) {
-  return cornersInTriangle(trianglePos).map((c) => vertForCorner(c, rect));
+export function getTriangleVerts(triangleRot: TriangleRotation, rect: Rect) {
+  return cornersInTriangle(triangleRot).map((c) => vertForCorner(c, rect));
 }
